61a9b435450c0e7de9ea75b4b07d82c4e2534929
Josh Brown <jbrown1618@github.com>
2024-06-18 22:01:34 -0400

    App communicates with dev backend

--- server/main.go
+++ server/main.go
@@ -1,17 +1,21 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func main() {
     http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Welcome to my website!")
+		fmt.Println("Received request from the mobile app")
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+        fmt.Fprintf(w, "{ \"message\": \"Welcome to my website!\" }")
     })
 
     fs := http.FileServer(http.Dir("static/"))
     http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-    http.ListenAndServe(":80", nil)
+	fmt.Println("Listening on port 8082")
+    http.ListenAndServe(":8082", nil)
 }
