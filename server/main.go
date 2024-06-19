15ea122d698454e5300d1b1fc48791b24bc3ebea
Josh Brown <jbrown1618@github.com>
2024-06-19 00:08:10 -0400

    Add environment configuration for server development

--- server/main.go
+++ server/main.go
@@ -3,19 +3,32 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Println(fmt.Errorf("error loading environment variables: %w", err).Error())
+	}
+
+	port := os.Getenv("PORT")
+	isDev := os.Getenv("ENVIRONMENT_TYPE") == "DEV"
+	
     http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Received request from the mobile app")
 		w.Header().Set("Content-Type", "application/json")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		if isDev {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+		}
         fmt.Fprintf(w, "{ \"message\": \"Welcome to my website!\" }")
     })
 
     fs := http.FileServer(http.Dir("static/"))
     http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	fmt.Println("Listening on port 8082")
-    http.ListenAndServe(":8082", nil)
+	fmt.Println("Listening on port " + port)
+    http.ListenAndServe(":" + port, nil)
 }
