33a6b18229965145517efddab5bb8dd4768a73fb
Josh Brown <jbrown1618@github.com>
2024-07-02 12:36:33 -0400

    Add dev database

--- server/go.mod
+++ server/go.mod
@@ -2,4 +2,7 @@ module jbrown1618/baby-stats
 
 go 1.22.4
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/mattn/go-sqlite3 v1.14.22
+)
--- server/main.go
+++ server/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
@@ -16,19 +18,32 @@ func main() {
 
 	port := os.Getenv("PORT")
 	isDev := os.Getenv("ENVIRONMENT_TYPE") == "DEV"
-	
-    http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+
+	db, err := sql.Open("sqlite3", "file:dev.db")
+	if err != nil {
+		fmt.Println(fmt.Errorf("error connecting to dev database: %w", err).Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Println(fmt.Errorf("error pinging dev database: %w", err).Error())
+		os.Exit(1)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Received request from the mobile app")
 		w.Header().Set("Content-Type", "application/json")
 		if isDev {
 			w.Header().Set("Access-Control-Allow-Origin", "*")
 		}
-        fmt.Fprintf(w, "{ \"message\": \"Welcome to my website!\" }")
-    })
+		fmt.Fprintf(w, "{ \"message\": \"Welcome to my website!\" }")
+	})
 
-    fs := http.FileServer(http.Dir("static/"))
-    http.Handle("/static/", http.StripPrefix("/static/", fs))
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	fmt.Println("Listening on port " + port)
-    http.ListenAndServe(":" + port, nil)
+	http.ListenAndServe(":"+port, nil)
 }
