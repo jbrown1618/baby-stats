8855bc5c89a5729d5f1abf79d88ba15b00947a8d
Josh Brown <jbrown1618@github.com>
2024-07-07 23:16:55 -0400

    Add event button

--- mobile-app/app/[babyID]/(tabs)/_layout.tsx
+++ mobile-app/app/[babyID]/(tabs)/_layout.tsx
@@ -1,21 +1,36 @@
-import { Tabs, useLocalSearchParams } from "expo-router";
+import { ThemedText } from "@/components/ThemedText";
+import { Link, Tabs, useLocalSearchParams } from "expo-router";
 
 export default function TabsLayout() {
   const { babyID } = useLocalSearchParams();
   return (
-    <Tabs>
-      <Tabs.Screen
-        name="index"
-        options={{ headerShown: false, href: `/${babyID}` }}
-      />
-      <Tabs.Screen
-        name="food"
-        options={{ headerShown: false, href: `/${babyID}/food` }}
-      />
-      <Tabs.Screen
-        name="sleep"
-        options={{ headerShown: false, href: `/${babyID}/sleep` }}
-      />
-    </Tabs>
+    <>
+      <Tabs>
+        <Tabs.Screen
+          name="index"
+          options={{ headerShown: false, href: `/${babyID}` }}
+        />
+        <Tabs.Screen
+          name="food"
+          options={{ headerShown: false, href: `/${babyID}/food` }}
+        />
+        <Tabs.Screen
+          name="sleep"
+          options={{ headerShown: false, href: `/${babyID}/sleep` }}
+        />
+      </Tabs>
+      <Link
+        href={`/${babyID}/add-event`}
+        style={{
+          position: "absolute",
+          right: 10,
+          bottom: 60,
+          backgroundColor: "red",
+          padding: 10,
+        }}
+      >
+        <ThemedText style={{ color: "white" }}>Add Event</ThemedText>
+      </Link>
+    </>
   );
 }
--- mobile-app/app/[babyID]/add-event.tsx
+++ mobile-app/app/[babyID]/add-event.tsx
@@ -1,5 +1,5 @@
 import { ThemedText } from "@/components/ThemedText";
-import { useLocalSearchParams } from "expo-router";
+import { useLocalSearchParams, router } from "expo-router";
 import { View } from "react-native";
 
 export default function AddEvent() {
@@ -7,6 +7,7 @@ export default function AddEvent() {
   return (
     <View>
       <ThemedText>AddEvent: {JSON.stringify(p)}</ThemedText>
+      <ThemedText onPress={() => router.back()}>Dismiss</ThemedText>
     </View>
   );
 }
