16f64dbad65e92cdad08eb1ba133e573b867d3cb
Josh Brown <jbrown1618@github.com>
2024-07-10 11:20:06 -0400

    Move baby-related handlers together

--- server/handler/babies.go
+++ server/handler/babies.go
@@ -6,6 +6,9 @@ import (
 	"jbrown1618/baby-stats/database"
 	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 func BabiesHandler(db *database.ApplicationDatabase) http.HandlerFunc {
@@ -27,5 +30,36 @@ func BabiesHandler(db *database.ApplicationDatabase) http.HandlerFunc {
 		}
 
 		fmt.Fprint(w, string(babiesJson))
+	}
+}
+
+func BabyHandler(db *database.ApplicationDatabase) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		babyIDPath := mux.Vars(r)["babyID"]
+		babyID, err := strconv.ParseUint(babyIDPath, 10, 64)
+		if err != nil {
+			log.Printf("invalid baby ID %s: %s", babyIDPath, err.Error())
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("400 - invalid baby ID"))
+			return
+		}
+
+		baby, err := db.GetBaby(1, babyID)
+		if err != nil {
+			fmt.Printf("Error: %s", err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("500 - something went wrong"))
+			return
+		}
+
+		babyJson, err := json.Marshal(baby)
+		if err != nil {
+			log.Printf("Error: %s", err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("500 - something went wrong"))
+			return
+		}
+
+		fmt.Fprint(w, string(babyJson))
 	}
 }
